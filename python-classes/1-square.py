1e807f765c4adbb5af7cb05be514e4223b111867
ValPQT <valentinpasquiet@gmail.com>
2026-01-26 14:38:07 +0100

    feat : add file 1-main.py and 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines a class square
+"""
+class Square:
+    """
+    Initialize the size  of the class
+    """
+    def __init__(self, size):
+        self.__size = size
