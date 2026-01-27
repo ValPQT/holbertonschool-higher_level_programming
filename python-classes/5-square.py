3d787cfde19ae9d6a35df34003c4ae4241d9d08b
ValPQT <valentinpasquiet@gmail.com>
2026-01-27 14:35:17 +0100

    feat : add files 5-main.py and 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a class Square with size validation and area calculation."""
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter: return the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter: check if the size is an int and if size >= 0."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
+
+
