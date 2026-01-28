c1511a03c2f39f8cb7105a9254cdab512307d8aa
ValPQT <valentinpasquiet@gmail.com>
2026-01-28 14:54:04 +0100

    fix : udpate the doc for 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""
-Defines a class square
-"""
+"""Defines a class square"""
+
+
 class Square:
     """Initialize the size  of the class"""
+
     def __init__(self, size):
         self.__size = size
