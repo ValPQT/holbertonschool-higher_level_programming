8be924bdeb4950b6a8289782141683af53cc12e0
ValPQT <valentinpasquiet@gmail.com>
2026-01-28 14:57:23 +0100

    fix : udpate the doc for 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-Defines a class square
-"""
+"""A class Square that defines a square by: (based on 1-square.py)"""
+
+
 class Square:
-    """ represents a square """
+    """Size must be an integer otherwaise raise a TypeError"""
     def __init__(self, size=0):
-        self.__size = int
+        """If size is less than 0 raise a ValueError"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        print(size)
-
+        self.__size = size
