2ed9543466572c89f9deaad94ad1543f93acd3e5
ValPQT <valentinpasquiet@gmail.com>
2026-01-28 14:59:16 +0100

    fix : update the doc for 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""
-Defines a class square with size validation and area method
-"""
+"""A class Square that defines a square by: (based on 2-square.py)"""
+
+
 class Square:
-    """represents a square """
+    """Size must be an integer otherwaise raise a TypeError"""
+
     def __init__(self, size=0):
+        """If size is less than 0 raise a ValueError"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = int
+        self.__size = size
 
     def area(self):
-        """Calculate and return the area of the square."""
-        return self.__size ** 2
+        """Returns the current square area"""
+        return self.__size * self.__size
