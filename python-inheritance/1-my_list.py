09df6515c312204ec41cf7717bda8a63a70ac866
ValPQT <valentinpasquiet@gmail.com>
2026-02-04 11:05:57 +0100

    fix : update the documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""Ce module définit une classe MyList qui hérite de la classe list et permet d’afficher la liste triée sans la modifier."""
+"""
+Ce module définit une classe MyList qui hérite de la classe list
+et permet d’afficher la liste triée sans la modifier.
+"""
 
 
 class MyList(list):
-    """Classe qui hérite de list et ajoute une méthode pour afficher la liste triée."""
-
+    """
+    Classe qui hérite de list et ajoute une méthode
+    pour afficher la liste triée.
+    """
     def print_sorted(self):
-        """ Affiche la liste triée par ordre croissant sans modifier la liste originale."""
+        """
+        Affiche la liste triée par ordre croissant
+        sans modifier la liste originale.
+        """
         print(sorted(self))
