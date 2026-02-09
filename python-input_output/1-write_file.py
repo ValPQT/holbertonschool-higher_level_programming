071e4ff102e4f6aa91b77c92e3d8aab6d105dc0d
ValPQT <valentinpasquiet@gmail.com>
2026-02-09 14:13:18 +0100

    fix : udpate documentation for 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+    Writes a string to a text file (UTF-8) and returns the number of characters written.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write into the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+
 
 def write_file(filename="", text=""):
     with open(filename, mode="w+", encoding="utf-8") as f:
