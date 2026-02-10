4ecf3db2c981a9a6d6ecd75e82c0fe41a2019177
ValPQT <valentinpasquiet@gmail.com>
2026-02-10 13:11:41 +0100

    fix : update the file 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, mode="w") as filename:
-        json.dump(my_obj, filename)
+    with open(filename, mode="w") as f:
+        json.dump(my_obj, f)
