a513e41a68e5f6df082dba05f02ca02a6ba813ed
ValPQT <valentinpasquiet@gmail.com>
2026-02-11 10:42:08 +0100

    fix : correct the output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,10 +7,9 @@ and print its content to standard output.
 
 def read_file(filename=""):
     """
-      Reads a text file (UTF-8) and prints its contents to stdout.
+    Reads a text file (UTF-8) and prints its contents to stdout.
 
-      Args:
-          filename (str): The name of the file to read.
-      """
+    Args: filename (str): The name of the file to read.
+    """
     with open(filename, mode="r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
