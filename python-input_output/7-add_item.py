66561337a3f9661cc076a96904ea45f031a00c1c
ValPQT <valentinpasquiet@gmail.com>
2026-02-11 10:59:57 +0100

    fix : update the documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+This script adds all command-line arguments to a JSON file (add_item.json).
+It loads the existing list from the file, appends new arguments, and saves
+the updated list back. The JSON file is created if it does not exist.
+"""
+
 import sys
 import json
 from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+"""
+Main function to add command-line arguments to a JSON file.
+"""
 args = sys.argv
 if path.exists('add_item.json'):
     list = load_from_json_file('add_item.json')
@@ -16,5 +24,3 @@ if len(sys.argv) >= 2:
     for i in range(1, len(sys.argv)):
         list.append(sys.argv[i])
 save_to_json_file(list, 'add_item.json')
-
-
