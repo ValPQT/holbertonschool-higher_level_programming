5856fb055e780cd56b4811e0d20322f673448429
ValPQT <valentinpasquiet@gmail.com>
2026-02-11 10:40:37 +0100

    fix : update the documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
+This module provides a function to read a UTF-8 encoded text file
+and print its content to standard output.
+"""
+
+
+def read_file(filename=""):
+    """
       Reads a text file (UTF-8) and prints its contents to stdout.
 
       Args:
           filename (str): The name of the file to read.
       """
-
-
-def read_file(filename=""):
     with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read())
