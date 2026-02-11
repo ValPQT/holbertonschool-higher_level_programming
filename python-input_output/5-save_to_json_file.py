abe7b46e5cb7551ee3e617a7f48d782937b1d9e7
ValPQT <valentinpasquiet@gmail.com>
2026-02-11 10:55:48 +0100

    fix : update the documentation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-import json
-
 """
-       Writes a Python object to a text file using JSON representation.
-
-       Args:
-           my_obj: The Python object to be serialized and written.
-           filename: The name of the file to write to.
-       """
+This module provides a function that appends a string
+to the end of a UTF-8 encoded text file.
+"""
+import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Writes a Python object to a text file using JSON representation.
+
+    Args:
+        my_obj: The Python object to save.
+        filename (str): The name of the file.
+    """
+
     with open(filename, mode="w") as f:
         json.dump(my_obj, f)
