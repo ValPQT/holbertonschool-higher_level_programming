3a16db6c18f6c68a12b19f28df3f600c909d3fb7
ValPQT <valentinpasquiet@gmail.com>
2026-02-18 22:07:37 +0100

    fix : update the output

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -10,26 +10,26 @@ def fetch_and_print_posts():
     response = requests.get(url)
     print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
-        data = response.json()
+        posts = response.json()
 
-        for post in data:
+        for post in posts:
             print(post['title'])
 
 
 def fetch_and_save_posts():
     response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
+        posts = response.json()
 
         cleaned_posts = []
-        for post in data:
+        for post in posts:
             cleaned_posts.append({
                 "id": post["id"],
                 "title": post["title"],
                 "body": post["body"]
             })
 
-        with open('data.csv', 'w', encoding='utf-8') as f:
+        with open('posts.csv', 'w', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=['id', 'title', 'body'])
             writer.writeheader()
             writer.writerows(cleaned_posts)
