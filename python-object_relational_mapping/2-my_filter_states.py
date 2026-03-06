26d2c5884797be0f94e14de7d3c912bc138322bd
ValPQT <valentinpasquiet@gmail.com>
2026-03-06 15:04:21 +0100

    fix : update the file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-"""Script that displays all states matching the name argument."""
+"""List states from the database whose name matches the user input."""
+
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
     )
+
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-            sys.argv[4]
-        )
-    )
+
+    query = (
+        "SELECT states.id, states.name "
+        "FROM states "
+        "WHERE name = BINARY '{}' "
+        "ORDER BY states.id ASC"
+    ).format(state)
+
+    cursor.execute(query)
+
     rows = cursor.fetchall()
+
     for row in rows:
         print(row)
+
     cursor.close()
+
     db.close()
