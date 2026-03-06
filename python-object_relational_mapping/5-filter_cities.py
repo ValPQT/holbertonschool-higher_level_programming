8c7802dfdb43fc47f04cfa9c2662fc3c0247c79f
ValPQT <valentinpasquiet@gmail.com>
2026-03-06 15:39:21 +0100

    fix : update the output

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-"""Script that lists all cities of a state from the database hbtn_0e_4_usa."""
+"""List all cities of a given state from the database,
+safely and ordered by cities.id."""
+
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
     )
+
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT cities.name FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s ORDER BY cities.id ASC",
-        (sys.argv[4],)
+
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "INNER JOIN states "
+        "ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
     )
+
+    cursor.execute(query, (state,))
+
     rows = cursor.fetchall()
-    print(", ".join(row[0] for row in rows))
+
+    print(", ".join([row[0] for row in rows]))
+
     cursor.close()
+
     db.close()
