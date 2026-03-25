d575615958584310dc1f0c7e59f809ee25b7c7a9
ValPQT <valentinpasquiet@gmail.com>
2026-03-25 10:55:17 +0100

    feat : add 3-script.js and 3-main.html

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const toggle = document.querySelector('#toggle_header');
+
+toggle.addEventListener('click', () => {
+  if (header.classList.contains('red')) {
+    header.classList.replace('red', 'green');
+  } else {
+    header.classList.replace('green', 'red');
+  }
+});
